6e6567675602fccbd5d0cbb8b5c18345807c4c9f
Luciano Vieira Rodrigues <lvrodriguesline@gmail.com>
2022-01-08 12:05:52 -0300

    Persistência de configuração.

---
+++ src/main/java/br/com/spiderbot/wizard/Constants.java
@@ -0,0 +1,28 @@
+package br.com.spiderbot.wizard;
+
+/**
+* Constantes compartilhadas entre as páginas para troca de parâmetros.
+*
+* @author <a href="mailto:lvrodrigues@spiderbot.com.br">Luciano Vieira Rodrigues</a>
+* @since 08/01/2022
+* @author $$Author$$
+* @version $$Revision$$ - $$Date$$
+*/
+public final class Constants {
+
+    /**
+     * Parâmetro nome.
+     */
+    public static final String PARAM_NAME = "name";
+
+    /**
+     * Parâmetro contador.
+     */
+    public static final String PARAM_COUNTER = "counter";    
+
+    /**
+     * Construtor oculto.
+     */
+    private Constants() {
+    }
+}
--- src/main/java/br/com/spiderbot/wizard/pages/Parameters.java
+++ src/main/java/br/com/spiderbot/wizard/pages/Parameters.java
@@ -1,5 +1,6 @@
 package br.com.spiderbot.wizard.pages;
 
+import br.com.spiderbot.wizard.Constants;
 import javafx.fxml.FXML;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
@@ -16,20 +17,10 @@ import javafx.scene.control.TextField;
 public class Parameters extends Page {
 
     /**
-     * Parâmetro nome.
-     */
-    public static final String PARAM_NAME = "name";
-
-    /**
      * Valor padrão para o parâmetro <b>name</b> quando este ainda não
      * estiver atribuído.
      */
     private static final String PARAM_NAME_DEFAULT = "";
-
-    /**
-     * Parâmetro contador.
-     */
-    public static final String PARAM_COUNTER = "counter";
 
     /**
      * Valor padrão para o parâmetro <b>counter</b> quando este ainda
@@ -58,16 +49,16 @@ public class Parameters extends Page {
 
     @Override
     public void onHide() {
-        datas().put(PARAM_NAME, fieldName.getText());
-        datas().put(PARAM_COUNTER, fieldCounter.getValue().toString());
+        datas().put(Constants.PARAM_NAME, fieldName.getText());
+        datas().put(Constants.PARAM_COUNTER, fieldCounter.getValue().toString());
         super.onHide();
     }
 
     @Override
     public void onShow() {
         super.onShow();
-        fieldName.setText(datas().getOrDefault(PARAM_NAME, PARAM_NAME_DEFAULT));
-        fieldCounter.getValueFactory().setValue(Integer.valueOf(datas().getOrDefault(PARAM_COUNTER, PARAM_COUNTER_DEFAULT.toString())));
+        fieldName.setText(datas().getOrDefault(Constants.PARAM_NAME, PARAM_NAME_DEFAULT));
+        fieldCounter.getValueFactory().setValue(Integer.valueOf(datas().getOrDefault(Constants.PARAM_COUNTER, PARAM_COUNTER_DEFAULT.toString())));
     }
 
     @Override
