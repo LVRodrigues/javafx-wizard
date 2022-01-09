c6d3c415bf0546ffa0d11a3c2667e1e981037d92
Luciano Vieira Rodrigues <lvrodriguesline@gmail.com>
2022-01-09 12:38:12 -0300

    Ajustes de uso de módulos.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,9 @@ module br.com.spiderbot.wizard {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.xml;
+    requires de.jensd.fx.glyphs.commons;
+    requires de.jensd.fx.glyphs.controls;    
+    requires de.jensd.fx.glyphs.fontawesome;
     opens br.com.spiderbot.wizard to javafx.fxml;
     opens br.com.spiderbot.wizard.pages to javafx.fxml;
     exports br.com.spiderbot.wizard;
